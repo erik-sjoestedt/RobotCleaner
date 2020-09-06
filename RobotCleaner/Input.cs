1626dbabe15a4ea0be5793c29752590dd2b6234e
erik.sjoestedt@hotmail.com <erik.sjoestedt@hotmail.com>
2020-09-06 18:10:03 +0200

    Parse start position input

---
+++ RobotCleaner/Input.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobotCleaner
+{
+    public class Input
+    {
+        private int startX;
+        private int startY;
+
+        public Input(string[] args)
+        {
+            string startPos = args[1];
+            string[] startPositions = startPos.Split(" ");
+            startX = Int32.Parse(startPositions[0]);
+            startY = Int32.Parse(startPositions[1]);
+        }
+
+        public int GetStartX()
+        {
+            return startX;
+        }
+        public int GetStartY()
+        {
+            return startY;
+        }
+    }
+}
---
+++ RobotCleanerTests/TestsInput.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using RobotCleaner;
+
+namespace RobotCleanerTests
+{
+    public class TestsInput
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestParseStartPosition()
+        {
+            string[] args = { "0", "4 5" };
+
+            Input input = new Input(args);
+
+            Assert.AreEqual(input.GetStartX(), 4);
+            Assert.AreEqual(input.GetStartY(), 5);
+        }
+    }
+}
