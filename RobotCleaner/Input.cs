fe36e4717fb34877ce128b8571072aba133c3e2b
erik.sjoestedt@hotmail.com <erik.sjoestedt@hotmail.com>
2020-09-06 20:01:16 +0200

    Parse (two) input move commands

--- RobotCleaner/Input.cs
+++ RobotCleaner/Input.cs
@@ -9,10 +9,12 @@ namespace RobotCleaner
     {
         private int startX;
         private int startY;
+        private readonly List<MoveCommand> moveCommands = new List<MoveCommand>();
 
         public Input(string[] args)
         {
             ParseStartPosition(args[1]);
+            ParseMoveCommands(args);
         }
 
         private void ParseStartPosition(string startPos)
@@ -20,6 +22,36 @@ namespace RobotCleaner
             string[] startPositions = startPos.Split(" ");
             startX = Int32.Parse(startPositions[0]);
             startY = Int32.Parse(startPositions[1]);
+        }
+
+        private void ParseMoveCommands(string[] args)
+        {
+            int numCommands = Int32.Parse(args[0]);
+            // move commands start at 2nd index
+            for (int i = 2; i < numCommands + 2; i++)
+            {
+                string[] moveCommandArgs = args[i].Split(" ");
+                Direction direction;
+                int numSteps = Int32.Parse(moveCommandArgs[1]);
+                switch (moveCommandArgs[0])
+                {
+                    case "N":
+                        direction = Direction.E;
+                        break;
+                    case "W":
+                        direction = Direction.W;
+                        break;
+                    case "S":
+                        direction = Direction.S;
+                        break;
+                    case "E":
+                        direction = Direction.E;
+                        break;
+                    default:
+                        throw new Exception("Direction not supported");
+                }
+                moveCommands.Add(new MoveCommand(direction, numSteps));
+            }
         }
 
         public int GetStartX()
@@ -30,5 +62,34 @@ namespace RobotCleaner
         {
             return startY;
         }
+
+        public List<MoveCommand> GetMoveCommands() { 
+            return moveCommands; 
+        }
+    }
+
+    public struct MoveCommand
+    {
+        private Direction direction;
+        private int numSteps;
+
+        public MoveCommand(Direction direction, int numSteps)
+        {
+            this.direction = direction;
+            this.numSteps = numSteps;
+        }
+
+        public Direction GetDirection() {
+            return direction;
+        }
+
+        public int GetNumSteps() {
+            return numSteps;
+        }
+    }
+
+    public enum Direction
+    {
+        N, W, S, E
     }
 }
--- RobotCleanerTests/TestsInput.cs
+++ RobotCleanerTests/TestsInput.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using RobotCleaner;
+using System.Collections.Generic;
 
 namespace RobotCleanerTests
 {
@@ -30,6 +31,21 @@ namespace RobotCleanerTests
 
             Assert.AreEqual(input.GetStartX(), -4);
             Assert.AreEqual(input.GetStartY(), 5);
+        }
+
+        [Test]
+        public void TestParseTwoMoveCommands()
+        {
+            string[] args = { "2", "0 0", "E 1", "S 2"};
+
+            Input input = new Input(args);
+            List<MoveCommand> moveCommands = input.GetMoveCommands();
+
+            Assert.AreEqual(moveCommands[0].GetDirection(), Direction.E);
+            Assert.AreEqual(moveCommands[0].GetNumSteps(), 1);
+
+            Assert.AreEqual(moveCommands[1].GetDirection(), Direction.S);
+            Assert.AreEqual(moveCommands[1].GetNumSteps(), 2);
         }
     }
 }
