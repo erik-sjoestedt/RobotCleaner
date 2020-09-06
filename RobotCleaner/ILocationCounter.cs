f807fabde75d94f2bfd385f469a7115c1238f062
erik.sjoestedt@hotmail.com <erik.sjoestedt@hotmail.com>
2020-09-06 21:14:25 +0200

    Add LocationCounter interface

---
+++ RobotCleaner/ILocationCounter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobotCleaner
+{
+    interface ILocationCounter
+    {
+        public int CountDistinct();
+    }
+}
