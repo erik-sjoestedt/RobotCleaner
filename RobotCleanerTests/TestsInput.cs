5b0bd702cef2f3403f2424b72ae160881539e9db
erik.sjoestedt@hotmail.com <erik.sjoestedt@hotmail.com>
2020-09-06 18:12:39 +0200

    Parse negative X start pos

--- RobotCleanerTests/TestsInput.cs
+++ RobotCleanerTests/TestsInput.cs
@@ -20,5 +20,16 @@ namespace RobotCleanerTests
             Assert.AreEqual(input.GetStartX(), 4);
             Assert.AreEqual(input.GetStartY(), 5);
         }
+
+        [Test]
+        public void TestParseStartPositionNegativeX()
+        {
+            string[] args = { "0", "-4 5" };
+
+            Input input = new Input(args);
+
+            Assert.AreEqual(input.GetStartX(), -4);
+            Assert.AreEqual(input.GetStartY(), 5);
+        }
     }
 }
